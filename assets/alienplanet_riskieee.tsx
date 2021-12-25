6f63afa540f9c9e6acdc8dc953e5f1102d311685
riskieee <git@riskieee.de>
2021-12-25 09:05:48 +0100

    Add first testchange.

---
+++ assets/alienplanet_riskieee.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="AlienPlanet_riskieee" tilewidth="32" tileheight="32" tilecount="192" columns="8" backgroundcolor="#005500">
+ <image source="alienplanet_riskieee.png" width="256" height="768"/>
+</tileset>
